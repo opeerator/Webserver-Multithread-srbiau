fc710da6209292583f2026d09a00314379b2e7a4
Ali Yamini <d.aliyamini@gmail.com>
2020-06-09 00:44:58 +0430

    adding packages

---
+++ webserver_multithreaded.py
@@ -0,0 +1,6 @@
+# This is my multithreaded project for OS class at srbiau with Ms.zamani
+# Author: Ali Yamini
+
+from threading import Thread 
+import socket 
+from socketserver import ThreadingMixIn
