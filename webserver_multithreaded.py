887bac7e26ccf42b9da1a972be7f3e2ac26f65c8
Ali Yamini <d.aliyamini@gmail.com>
2020-06-10 01:57:08 +0430

    adding socket for tcp server.

--- webserver_multithreaded.py
+++ webserver_multithreaded.py
@@ -4,3 +4,16 @@
 from threading import Thread 
 import socket 
 from socketserver import ThreadingMixIn
+
+
+class CliThread(Thread): 
+
+    # My socket for tcp server
+    BUFFER_SIZE = 20
+    TCP_IP = '0.0.0.0' 
+    TCP_PORT = 2004 
+
+    mytcpserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    mytcpserver.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+    mytcpserver.bind((TCP_IP, TCP_PORT)) 
+    threads = [] 
