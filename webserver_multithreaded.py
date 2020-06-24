56b8ea5b16e1d2c42ac6b6a6cf54b3a184116968
Ali Yamini <d.aliyamini@gmail.com>
2020-06-25 01:07:15 +0430

    adding _init_ for defining socket.

--- webserver_multithreaded.py
+++ webserver_multithreaded.py
@@ -8,11 +8,17 @@ from socketserver import ThreadingMixIn
 
 class CliThread(Thread): 
 
+    def __init__(self,ip,port): 
+        Thread.__init__(self) 
+        self.ip = ip 
+        self.port = port 
+        print "[+] New server socket thread started for " + ip + ":" + str(port) 
+        
     # My socket for tcp server
     BUFFER_SIZE = 20
     TCP_IP = '0.0.0.0' 
     TCP_PORT = 2004 
-
+    
     mytcpserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     mytcpserver.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
     mytcpserver.bind((TCP_IP, TCP_PORT)) 
